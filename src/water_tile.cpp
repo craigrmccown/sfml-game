984b61f31c09969c76c9e52ceb3439ad89cab677
Craig McCown <craigrmccown@gmail.com>
2016-09-11 15:19:26 -0400

    calculating sprite sheet based on set width and size

--- src/water_tile.cpp
+++ src/water_tile.cpp
@@ -2,7 +2,7 @@
 
 WaterTile::WaterTile(sf::Texture *texture)
 {
-    this->sprite_sheet = new SpriteSheet(texture, 100, 50);
+    this->sprite_sheet = new SpriteSheet(texture, this->width, this->depth + this->height);
     this->animation = new Animation(this->sprite_sheet, 0, 5, 1000);
     EventBus::subscribe(sf::Event::EventType::KeyPressed, this, &WaterTile::handle_key_pressed);
 }
